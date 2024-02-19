3ddecd30b04b08c9c842a0f04d621f3ad13629da
Peace44 <peacebonerabose@gmail.com>
2024-02-19 10:58:15 +0100

    Updated the bash script!

--- BASIC_DBSCAN/basic_dbscan.sh
+++ BASIC_DBSCAN/basic_dbscan.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
+# Get the directory where the script is located
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Get the paths of the program & the exe
+PROG_CPP=$SCRIPT_DIR/basic_dbscan.cpp
+PROG=$SCRIPT_DIR/basic_dbscan
+
 # Compile the basic_dbscan.cpp program
-g++ -O3 basic_dbscan.cpp -o basic_dbscan -std=c++17
+g++ -O3 $PROG_CPP -o $PROG -std=c++17
 
 run() {
     dataset_name=$1
     output_file=$2
 
+    # Start time in millisecs
+    start=$(date +%s%3N)
+
     # Run the program and capture the output
-    ./basic_dbscan $dataset_name >> $output_file
+    $PROG $dataset_name >> $output_file
+
+    # End time in millisecs
+    end=$(date +%s%3N)
+
+    # Calculate exec time
+    exec_time=$((end - start))
+
+    # Append exec time to the output file
+    echo "Execution time for dataset $dataset_name: $exec_time ms" >> $output_file
 }
 
 # Output file
-output_file="basic_dbscan.txt"
+output_file="$SCRIPT_DIR/basic_dbscan.txt"
+input_file="$SCRIPT_DIR/../INPUTS/random_points.csv"
 
 # Ensure the output file is empty
 > $output_file
 
 # Run benchmarks
-run "../INPUTS/random_points.csv" $output_file
+run $input_file $output_file
 
 echo "Check the '$output_file' file!"
