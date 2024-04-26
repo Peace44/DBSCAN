88b1f05f4bbb9fd44260fad1ecf3577af33e79d7
mickp18 <miperseo@gmail.com>
2024-04-26 13:27:45 +0200

    add input file generation

--- compare.sh
+++ compare.sh
@@ -2,7 +2,21 @@
 
 # Define an array with script names
 scripts=("BASIC_DBSCAN/basic_dbscan.sh"  "KDTREE/KDTREE.sh" "MLPACK_DBSCAN/mlpack_dbscan.sh")
+
+# generate random points : NUM_CLUSTERS * POINTS_PER_CLUSTER = number of points generated 
+NUM_CLUSTERS=50
+POINTS_PER_CLUSTER=2000
+
+# Generate random points
+echo "Generating the random points input file..."
+python3 points_generator.py --num_clusters $NUM_CLUSTERS --points_per_cluster $POINTS_PER_CLUSTER
+echo "Random points generated."
+
+# Define input file
 input_file="../INPUTS/random_points.csv"
+
+
+# Set default values of DBSCAN parameters: eps and minPts
 eps=2.0
 minPts=2
 
@@ -10,6 +24,10 @@ minPts=2
 > compare.txt
 
 echo "Ensuring all scripts are executable..."
+echo "Generating results using $NUM_CLUSTERS clusters and $POINTS_PER_CLUSTER points per cluster" >> compare.txt
+echo "eps = $eps, minPts = $minPts" >> compare.txt
+echo "" >> compare.txt
+
 # Ensure scripts are executable
 for script in "${scripts[@]}"; do
     chmod +x "$script" || echo "Failed to set executable flag on $script"
