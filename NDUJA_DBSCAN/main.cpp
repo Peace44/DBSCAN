73ae5da3a4174f59592d9c6b1148ea36096df669
Michele <miperseo@gmail.com>
2024-05-30 23:04:41 +0200

    implement nduja_dbscan

---
+++ NDUJA_DBSCAN/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <charconv>
+#include <chrono>
+#include "dbscan.h"
+
+const int UNCLASSIFIED = 0;
+
+
+
+std::vector<Point> read_points_from_csv(const std::string& filename) {
+    std::vector<Point> points;
+    std::ifstream file(filename, std::ios::binary); // Open in binary mode to speed up reading
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return points; // Return an empty vector if the file cannot be opened
+    }
+
+    file.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Skip the first line (header)
+    
+    std::string line;
+    while (std::getline(file, line)) {
+        Point point;
+
+        char* end; // Temporary buffer for parsing numbers
+
+        point.x = std::strtod(line.c_str(), &end);
+        point.y = std::strtod(end + 1, &end);
+        point.z = std::strtod(end + 1, nullptr);
+
+        points.push_back(point);
+    }
+
+    return points;
+}
+
+
+
+void write_points_to_csv(const std::string& filename, const std::vector<std::vector<int>>& clusters) {
+    std::ofstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+
+    for (size_t i = 0; i < clusters.size(); ++i) {
+        file << "Cluster " << i << ":\n";
+        for (const auto& point : clusters[i]) {
+            file << point << "\n";
+        }
+        file << "\n";
+    }
+
+    file.close();
+}
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0] << " <input_filename> <eps> <min_pts>" << std::endl;
+        return 1;
+    }
+
+    std::string input_filename = argv[1];
+
+    // Generate output filename based on input filename
+    std::size_t dotPos = input_filename.rfind('.');
+    std::string baseName = (dotPos == std::string::npos) ? input_filename : input_filename.substr(0, dotPos);
+    std::string extension = (dotPos == std::string::npos) ? "" : input_filename.substr(dotPos);
+    
+    // Replace "INPUTS" with "OUTPUTS" in the baseName
+    std::size_t inputsPos = baseName.find("INPUTS");
+    if (inputsPos != std::string::npos) baseName.replace(inputsPos, strlen("INPUTS"), "OUTPUTS");
+    std::string output_filename = baseName + "_clusters" + extension;
+
+    // Read points from CSV
+    std::vector<Point> points = read_points_from_csv(input_filename);
+
+    // Parameters for DBSCAN
+    double eps = std::atoi(argv[2]); // Adjust based on your dataset
+    int min_pts = std::atoi(argv[3]); // Adjust based on your dataset
+
+    // Apply DBSCAN
+    DBSCAN dbscan(eps, min_pts, 0.0, 0.0, 0.0, 0.0, points);
+    auto start = std::chrono::high_resolution_clock::now(); // Before calling dbscan, get the starting time_point
+    dbscan.run();    
+    auto stop = std::chrono::high_resolution_clock::now(); // After dbscan completes, get the ending time_point
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    auto clusters = dbscan.getCluster(); 
+    std::cout << "NDUJA'S DBSCAN execution time: " << duration.count() << " milliseconds" << std::endl;
+    
+
+    // Write the clustered points to CSV
+    write_points_to_csv(output_filename, clusters);
+    // std::cout << "Clustering results have been written to " << output_filename << std::endl;
+    
+    return 0;
+}
---
+++ NDUJA_DBSCAN/nduja_dbscan.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+# Get the paths of the program & the exe
+PROG_CPP=$SCRIPT_DIR/main.cpp
+DBSCAN_CPP=$SCRIPT_DIR/nduja_dbscan.cpp
+PROG=$SCRIPT_DIR/nduja_dbscan
+
+# Compile the program
+g++ -O3 -I /usr/local/include/eigen3 $DBSCAN_CPP $PROG_CPP -o $PROG -std=c++17
+if [[ $? -ne 0 ]]; then
+    echo "Compilation failed."
+    exit 1
+fi
+
+run() {
+    dataset_name=$1
+    output_file=$2
+    eps=$3
+    minPts=$4
+    
+    # Start time in milliseconds
+    start=$(date +%s%3N)
+
+    # Run the program and capture the output
+    $PROG $dataset_name $eps $minPts | tee -a $output_file
+
+    # End time in milliseconds
+    end=$(date +%s%3N)
+
+    # Calculate exec time
+    exec_time=$((end - start))
+
+    # Append exec time to the output file
+    echo "Total execution time for dataset $dataset_name: $exec_time ms" | tee -a "$output_file"
+}
+
+# Ensure the correct number of arguments are provided
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 <input_file> <eps> <minPts>"
+    exit 1
+fi
+
+output_file=$SCRIPT_DIR/nduja_dbscan.txt
+input_file=$(realpath $SCRIPT_DIR/$1)
+eps=$2
+minPts=$3
+
+# Ensure the output file is empty
+> $output_file
+
+# Run benchmarks
+run $input_file $output_file $eps $minPts
+
+# echo "Completed. Check the '$output_file' file!"
