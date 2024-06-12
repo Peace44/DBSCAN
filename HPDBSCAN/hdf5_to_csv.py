241515aefd8128dbf2b2a91290376aaca246c69f
Peace44 <peacebonerabose@gmail.com>
2024-06-12 12:53:03 +0200

    ...

--- HPDBSCAN/hdf5_to_csv.py
+++ HPDBSCAN/hdf5_to_csv.py
@@ -3,18 +3,14 @@ import h5py
 import pandas as pd
 import sys
 
-def hdf5_to_csv(hdf5_file, csv_file):
+def hdf5_to_csv(hdf5_file, csv_file, key):
     try:
         with h5py.File(hdf5_file, 'r') as hdf:
-            # Assuming the dataset is located in the 'DATA' group
-            #data = hdf['DATA'][()]
-            clusters = hdf['CLUSTERS'][()]
-            # Convert to DataFrame
-            #data_df = pd.DataFrame(data)
-            clusters_df = pd.DataFrame(clusters)
-            # Save to CSV
-            #data_df.to_csv(csv_file, index=False)
-            clusters_df.to_csv(csv_file, index=False)
+            print("Keys: %s" % hdf.keys())
+            
+            data = hdf[key][()]
+            data_df = pd.DataFrame(data)
+            data_df.to_csv(csv_file, index=False)
             print(f"Successfully converted {hdf5_file} to {csv_file}")
     except KeyError as e:
         print(f"Key error: {e}")
@@ -22,9 +18,10 @@ def hdf5_to_csv(hdf5_file, csv_file):
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python hdf5_to_csv.py <input_hdf5_file> <output_csv_file>")
+    if len(sys.argv) != 4:
+        print("Usage: python hdf5_to_csv.py <input_hdf5_file> <output_csv_file> <key>")
     else:
         hdf5_file = sys.argv[1]
         csv_file = sys.argv[2]
-        hdf5_to_csv(hdf5_file, csv_file)
+        key = sys.argv[3]
+        hdf5_to_csv(hdf5_file, csv_file, key)
